0722088324bf534d25db627cb170250830ecf658
Seven <chanutpong.promsin@gmail.com>
2024-01-15 17:55:58 +0700

    🚀 : v1.0.0

--- defi_sdk_py/__init__.py
+++ defi_sdk_py/__init__.py
@@ -1,12 +1,5 @@
-import json
-import sys, os
-# from .address_const import AddressConst
-from .chain_client import ChainClient
-# from .fwx_chain import AddressConst as FWX_ADDRESS
-
-# project_dir = os.path.abspath("defi_sdk_py")
-# sys.path.append(project_dir)
-# with open(project_dir+"/address/fwx_address.json", 'r') as file:
-#     json_data = json.load(file)
-
-# FWX_AddressConst = AddressConst(json_data)
+# from .chain_client import ChainClient
+from .fwx_chain import FWXChainClient
+from .fwx_chain import ADDRESS as FWX_ADDRESS
+from .avax_fuji_chain import AVAXFUJIChainClient
+from .avax_fuji_chain import ADDRESS as FUJI_ADDRESS
