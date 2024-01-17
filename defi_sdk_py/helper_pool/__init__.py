77660e3a6d7a4a44dbf27a0e7d89edbf5e87e127
Seven <chanutpong.promsin@gmail.com>
2024-01-17 11:10:18 +0700

    🟠 : adjust helper_pool doc

--- defi_sdk_py/helper_pool/__init__.py
+++ defi_sdk_py/helper_pool/__init__.py
@@ -7,6 +7,12 @@ from ..utils import parseEther,Day
 from typing import List
 
 class ClaimableInterest:
+    """
+    Represents the claimable interest for a stake in a pool.
+
+    :param tokenInterest: The claimable token interest amount
+    :param forwInterest: The claimable forward interest amount
+    """
 
     def __init__(self, tokenInterest:int, forwInterest:int) -> None:
         self.tokenInterest = tokenInterest
@@ -19,6 +25,12 @@ class ClaimableInterest:
         })
 
 class BorrowingInterest:
+    """
+    Represents the borrowing interest details for a loan.
+
+    :param ltv: The loan-to-value ratio
+    :param interest: The interest amount
+    """
 
     def __init__(self, ltv:int, interest:int) -> None:
         self.ltv = ltv
@@ -31,6 +43,16 @@ class BorrowingInterest:
         })
 
 class PoolInfo:
+    """
+    Represents the information about a pool.
+
+    :param borrowing_interest: The interest rate for borrowing
+    :param lending_token_interest: The interest rate for token lending
+    :param lending_forw_interest: The interest rate for forward lending
+    :param utilization_rate: The utilization rate of the pool
+    :param p_token_total_supply: The total supply of the pool's P tokens
+    :param current_supply: The current supply of the pool
+    """
     def __init__(
         self,
         borrowing_interest: int,
@@ -58,6 +80,15 @@ class PoolInfo:
         })
 
 class LendingInfo:
+    """
+    Represents the lending information for a stake in a pool.
+
+    :param lending_balance: The lending balance
+    :param interest_token_gained: The interest gained in tokens
+    :param interest_forw_gained: The interest gained in forwards
+    :param rank: The rank of the lender
+    :param rank_info: The rank information
+    """
     def __init__(
         self,
         lending_balance: int,
@@ -82,20 +113,49 @@ class LendingInfo:
         })
 
 class HelperPool:
+    """
+    HelperPool provides utility functions to assist with pool-related calculations.
+    """
 
     def __init__(self):
+        """
+        Initializes the HelperPool by setting up the helper pool contract interface.
+        """
         super(HelperPool, self).__init__()
         self.helper_pool:IHelperPool = IHelperPool(self.address_const.get_helper_pool_address(), self.web3)
 
     def claimble_interest(self, pool:IAPHPool, nft_id:int)->ClaimableInterest:
+        """
+        Retrieves the claimable interest for a given NFT ID in a pool.
+
+        :param pool: The pool contract interface
+        :param nft_id: The NFT ID to query for claimable interest
+        :return: A ClaimableInterest object containing the token and forward interest amounts
+        """
         result = self.helper_pool.claimableInterest(pool.address, nft_id).call()
         return ClaimableInterest(result[0], result[1])
 
     def claimble_interest_membership(self, pool:IAPHPool, nft_id:int)->ClaimableInterest:
+        """
+        Retrieves the claimable interest for a given NFT ID in a pool, accounting for membership.
+
+        :param pool: The pool contract interface
+        :param nft_id: The NFT ID to query for claimable interest with membership considered
+        :return: A ClaimableInterest object containing the token and forward interest amounts
+        """
+
         result = self.helper_pool.claimableInterestMembership(pool.address, nft_id).call()
         return ClaimableInterest(result[0], result[1])
 
     def get_next_lending_forw_interest(self, pool:IAPHPool, new_deposit_amount:int, forw_price_rate:int)->int:
+        """
+        Calculates the next lending forward interest for a new deposit in a pool.
+
+        :param pool: The pool contract interface
+        :param new_deposit_amount: The new deposit amount
+        :param forw_price_rate: The price rate of the forward token
+        :return: The next lending forward interest amount
+        """
         pool_token = IERC20Metadata(pool.tokenAddress().call(), self.web3)
         forw_token = IERC20Metadata(pool.forwAddress().call(), self.web3)
         new_deposit_amount = parseEther(self.web3, new_deposit_amount, pool_token.decimals().call())
@@ -104,16 +164,41 @@ class HelperPool:
         return self.helper_pool.getNextLendingForwInterest(pool.address, new_deposit_amount, forw_price_rate, forw_precision).call()
 
     def get_next_lending_interest(self, pool:IAPHPool, new_deposit_amount:int)->int:
+        """
+        Calculates the next lending interest for a new deposit in a pool.
+
+        :param pool: The pool contract interface
+        :param new_deposit_amount: The new deposit amount
+        :return: The next lending interest amount
+        """
+
         pool_token = IERC20Metadata(pool.tokenAddress().call(), self.web3)
         new_deposit_amount = parseEther(self.web3, new_deposit_amount, pool_token.decimals().call())
         return self.helper_pool.getNextLendingInterest(pool.address, new_deposit_amount).call()
 
     def get_interest_amount_by_deposit_amount(self, pool:IAPHPool, deposit_amount:int, day_second:int=Day())->int:
+        """
+        Calculates the interest amount based on a deposit amount for a given time period in a pool.
+
+        :param pool: The pool contract interface
+        :param deposit_amount: The deposit amount
+        :param day_second: The time period in seconds for which the interest is calculated (default is one day)
+        :return: The interest amount for the deposit
+        """
+
         pool_token = IERC20Metadata(pool.tokenAddress().call(), self.web3)
         deposit_amount = parseEther(self.web3, deposit_amount, pool_token.decimals().call())
         return self.helper_pool.getInterestAmountByDepositAmount(pool.address, deposit_amount, day_second).call()
     
     def get_deposit_amount_by_interest_amount(self, pool:IAPHPool, interest_amount:int, day_second:int=Day())->int:
+        """
+        Calculates the deposit amount required to achieve a target interest amount for a given time period in a pool.
+
+        :param pool: The pool contract interface
+        :param interest_amount: The target interest amount
+        :param day_second: The time period in seconds for which the deposit is calculated (default is one day)
+        :return: The deposit amount required for the target interest
+        """
         pool_token = IERC20Metadata(pool.tokenAddress().call(), self.web3)
         interest_amount = parseEther(self.web3, interest_amount, pool_token.decimals().call())
         return self.helper_pool.getDepositAmountByInterestAmount(pool.address, interest_amount, day_second).call()
@@ -125,7 +210,16 @@ class HelperPool:
         collateral_amount:int,
         collateral_token:IERC20Metadata,
         day_second:int=Day())->BorrowingInterest:
+        """
+        Calculates the borrowing interest for a given loan amount and collateral in a pool.
 
+        :param pool: The pool contract interface
+        :param borrow_amount: The borrow amount
+        :param collateral_amount: The collateral amount
+        :param collateral_token: The collateral token metadata
+        :param day_second: The time period in seconds for which the interest is calculated (default is one day)
+        :return: A BorrowingInterest object containing the LTV and interest
+        """
         borrow_token = IERC20Metadata(pool.tokenAddress().call(), self.web3)
         borrow_amount = parseEther(self.web3, borrow_amount, borrow_token.decimals().call())
         collateral_amount = parseEther(self.web3, collateral_amount, collateral_token.decimals().call())
@@ -140,6 +234,13 @@ class HelperPool:
         return BorrowingInterest(*result)
     
     def get_pool_info(self, pool:IAPHPool, forw_price_rate:int)->PoolInfo:
+        """
+        Retrieves information about a pool, including interest rates, utilization rate, and supply details.
+
+        :param pool: The pool contract interface
+        :param forw_price_rate: The price rate of the forward token
+        :return: A PoolInfo object containing detailed information about the pool
+        """
         forw_token = IERC20Metadata(pool.forwAddress().call(), self.web3)
         forw_price_rate = parseEther(self.web3, forw_price_rate, forw_token.decimals().call())
         forw_precision = parseEther(self.web3, 1, forw_token.decimals().call())
@@ -147,6 +248,13 @@ class HelperPool:
         return PoolInfo(*result)
 
     def get_lending_info(self, pool:IAPHPool, nft_id:int)->LendingInfo:
+        """
+        Retrieves lending information for a given NFT ID in a pool, including balance, interest gained, and rank.
+
+        :param pool: The pool contract interface
+        :param nft_id: The NFT ID to query for lending information
+        :return: A LendingInfo object containing detailed information about the lending stake
+        """
         result = self.helper_pool.getLendingInfo(pool.address, nft_id).call()
         return LendingInfo(*result[:4], rank_info=RankInfo(*result[4]))
     
